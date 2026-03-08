2dba9e9c297b3cb23f18c3eebc057697b795e5bd
Kaabiee <caremi1u7@gmail.com>
2026-03-07 22:55:12 -0600

    Update IMC calculator output

--- imc.cpp
+++ imc.cpp
@@ -3,17 +3,36 @@ using namespace std;
 
 int main() {
     cout << "Hola, bienvenido a la calculadora imc." << endl;
-    double peso, altura, imc;
+    double peso, altura, imc; // es como un float pero mas preciso?
 
     cout << "Primero ingresa tu peso." << endl;
     cin >> peso;
 
-    cout << "Ahora tu altura" << endl;
+    cout << "Ahora tu altura en metros." << endl;
     cin >> altura;
+    if (altura <= 0) {
+        cout << "Tu estatura no es valida." << endl;
+    }
 
     imc = peso / (altura * altura);
 
     cout << "Tu imc es: " << imc << endl;
 
+    if (imc < 18.5) {                 // es igual que python casi casi solo cambia el uso de llaves y parentesis.
+        cout << "Tu peso esta bajo." << endl;
+    }
+
+    else if (imc < 25) {       // es el elif basicamente. 
+        cout << "Tu peso esta bien." << endl;
+    }
+
+    else if (imc < 30) {
+        cout << "Tienes sobre peso." << endl;
+    }
+
+    else {
+        cout << "Tienes obesidad." << endl;
+    }
+ 
     return 0;
 }
